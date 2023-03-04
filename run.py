bf25bcbecabf71e2ed240505f8f4821b85f624ef
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-03-04 20:23:40 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -71,12 +71,19 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+   
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+       surplus = int(stock) - sales
+       surplus_data.append(surplus)
+    print(surplus_data)
+
+       
 
 
 def main():
     """
-    Run all functions
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
